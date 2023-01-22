d4be7db0fe742bea350fb3d7dd7d0aebc488c0f8
chandan kumar <ck893607@gmail.com>
2023-01-22 18:49:36 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,56 @@
+const express=require("express")
+const {connection}=require("./config/db")
+require("dotenv").config
+const{userrouter}=require("./routees/userrouter")
+const{adminproduct}=require("./routees/adminproduct.router")
+const {userrender}=require("./routees/userrenderrouter")
+const {userCartrouter}=require("./routees/usercartrouter")
+const {userwishlistrouter}=require("./routees/userwishlistrouter")
+const {adminRouter}=require("./routees/admin.router")
+const{authentication}=require("./middlewares/authenticationmiddleware")
+const cors = require('cors')
+
+
+const app=express()
+app.use(cors({
+    origin:"*"
+}))
+app.use(express.json())
+
+app.get("/",(req,res)=>
+{
+    res.send({"message":"welcome to HOME PAGE"})
+})
+app.use("/admin",adminRouter)
+
+app.use("/usersrender", userrender)
+app.use("/adminproducts",adminproduct)
+
+// app.use("user")
+
+app.use("/users",userrouter)
+
+
+
+app.use(authentication)
+app.use("/cart",userCartrouter)
+app.use("/wishlist",userwishlistrouter)
+
+
+
+
+
+
+
+
+
+app.listen(process.env.port, async()=>
+{
+    try {
+        await connection
+        console.log("connected to data base")
+    } catch (error) {
+        console.log(error)
+    }
+console.log(`server is running at port ${process.env.port}`)
+})
---
+++ middlewares/authenticationmiddleware.js
@@ -0,0 +1,31 @@
+var jwt = require('jsonwebtoken');
+
+
+const authentication=(req,res,next)=>
+{
+    const token =req.headers.authorization
+    console.log(token)
+    if(token)
+    {
+        const verified_token=jwt.verify(token,"masai")
+            console.log(verified_token)
+        if(verified_token)
+        {
+            const userID=verified_token.userID
+            console.log(userID)
+            req.body.userID=userID
+            next()
+        }
+        else{
+            res.send("please login first")
+        }
+    }
+    else{
+        res.send("please login")
+    }
+}
+
+
+module.exports={
+    authentication
+}
